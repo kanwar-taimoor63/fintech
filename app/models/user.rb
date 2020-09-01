6dc4176acab0fef50e98d9b87c8bb1e5b4129715
kanwar-taimoor63 <kanwar.taimoor@square63.org>
2020-09-01 18:53:08 +0500

    login commit before feedback

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,34 +1,35 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validate :password_complexity
   #validate :validate_username
+  validate :password_complexity
   validates :username, uniqueness: { case_sensitive: false }
 
-  before_validation  :add_username_in_db
+  before_validation :add_username_in_db
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   attr_writer :login
 
+#function to create a login attribute
   def login
     @login || self.username || self.email
   end
-
+#function to combine first name and last name in database as username
   def add_username_in_db
     self.username = self.firstname + " " + self.lastname
   end
-
+#over ridden function of devise to allow username for authentication 
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
       where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-    
+  
     elsif conditions.key?(:username) || conditions.key?(:email)
       where(conditions.to_hash).first
     end
   end
-
+#custom password validation
   def password_complexity
     rules = {
               " must contain at least one lowercase letter"  => /[a-z]+/,
@@ -44,7 +45,7 @@ class User < ApplicationRecord
   end
 end
 
-# def validate_username 
+# def validate_username
 #     if User.where(username: username).exists?
 #       errors.add(:username, :invalid)
 #     end
