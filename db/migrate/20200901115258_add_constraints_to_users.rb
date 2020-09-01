7e549b04ed49339980d213fe1e051e065b940f30
kanwar-taimoor63 <kanwar.taimoor@square63.org>
2020-09-01 17:43:48 +0500

    task login complete2

---
+++ db/migrate/20200901115258_add_constraints_to_users.rb
@@ -0,0 +1,7 @@
+class AddConstraintsToUsers < ActiveRecord::Migration[5.2]
+  def change
+  	change_column :users, :username, :string, null: false, default: ""
+  	change_column :users, :firstname, :string, null:false, default: ""
+    change_column :users, :lastname, :string, null:false, default: ""
+  end
+end
