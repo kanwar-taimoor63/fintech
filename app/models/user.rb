bf81d383ff8235cf30b9cefd9cd7735a6d38f85f
kanwar-taimoor63 <kanwar.taimoor@square63.org>
2020-09-01 17:43:11 +0500

    task login complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,54 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validate :password_complexity
+  validate :validate_username
+  before_save :add_username_in_db
+  attr_writer :login
+  validates :username, uniqueness: { case_sensitive: false }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
- def username
-    self.first_name + " " + self.last_name
- end
+def add_username_in_db
+	self.username= self.firstname + " " + self.lastname
+end
+
+def validate_username
+  if User.where(email: username).exists?
+    errors.add(:username, :invalid)
+  end
+end
+ 
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      elsif conditions.has_key?(:username) || conditions.has_key?(:email)#conditions[:email].downcase! if conditions[:email]
+        where(conditions.to_hash).first
+      end
+    end
+def password_complexity
+rules = {
+    " must contain at least one lowercase letter"  => /[a-z]+/,
+    " must contain at least one uppercase letter"  => /[A-Z]+/,
+    " must contain at least one digit"             => /\d+/,
+    " must contain at least one special character" => /[^A-Za-z0-9]+/,
+    " Length must be of atleast 8 characters" => /.{8,}$/
+
+  }
+
+  rules.each do |message, regex|
+    errors.add( :password, message ) unless password.match( regex )
+  end
+
+
+  
+
+end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_092525) do
+ActiveRecord::Schema.define(version: 2020_09_01_121606) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,8 +20,9 @@ ActiveRecord::Schema.define(version: 2020_08_31_092525) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "firstname"
-    t.string "lastname"
+    t.string "firstname", default: "", null: false
+    t.string "lastname", default: "", null: false
+    t.string "username", default: ""
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
