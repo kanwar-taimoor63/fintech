39103f3bfff5ba9b423783825f9e0adea908134a
kanwar-taimoor63 <kanwar.taimoor@square63.org>
2020-09-18 17:43:49 +0500

    orders index page+ nav bar for index pages

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,11 @@
 class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
-  
+    PER_PAGE=1
+  def self.search(search)
+    return all if search.blank?
+    where('orders.id LIKE ? OR orders.email LIKE ? OR orders.firstname LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%")
+  end
+
   def calculate_subtotal
     order_items.collect {|order_item| order_item.valid? ? (order_item.unit_price*order_item.quantity) : 0}.sum
   end
